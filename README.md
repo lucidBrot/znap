9f827a849cf15aba64c71fdc72969197ea751e9e
Eric Mink <eric@mink.li>
2020-05-21 20:44:58 +0200

    write messages to file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 znap -t tank/ds1/u18 -m COMMIT_MESSAGE
 ```
 
-Creates a snapshot of the dataset `tank/Ds1/u18` and stores `COMMIT_MESSAGE` in a file at `/opt/znap`.
+Creates a snapshot of the dataset `tank/ds1/u18` and stores `COMMIT_MESSAGE` in a file at `/opt/znap`.
 
 
 
@@ -14,3 +14,16 @@ Opens a text editor for that file.
 
 ### Why?
 Because I really don't want to type the date command again and again.
+And `znap` keeps track of commit messages in a file per dataset:
+
+```
+tank/ds1/u18@2005082034     swap partition instead of /swapfile.
+							hibernate enabled but without "nomodeset" yet.
+							Not yet tested.
+
+tank/ds1/u18@2005091133 	Setup Telegram.
+							Hibernation seems to be working perfectly.
+							But they say nomodeset can cause performance hits.
+							`systemctl suspend` seems to leave the screen on?
+
+```
--- znap.sh
+++ znap.sh
@@ -2,21 +2,34 @@
 # (c) LucidBrot 2020
 set -e
 
-# --- Customizable Options
-ZNAPLOGFILE='/opt/znap/messages.txt'            # where the logs are stored
+# --- Customizable Options ---
+ZNAPLOGFILE='.znap_log'                         # where the logs are stored
 SUFFIX='@'`date +\%y\%m\%d\%H\%M`               # see `man date` for format or provide any other suffix
 DEFAULT_DATASET='tank/ds1'                      # set to target dataset to take snapshot of
-RECURSIVE=1                                     # set to 0 if you want a non-recursive snapshot
+
+# --- Programming Options ---
+# set to sudo when used on a platform that has sudo
+sudo=''
+SEP='\t'
+BIGSEP='\n'
 
 # --- Functions ---
 
 usage(){
 # cat << EOF  means that cat sould stop reading when EOF is detected
 cat << EOF
+
 Usage:
-    znap -t tank/DATASET -m "COMMIT_MESSAGE"        creates a snapshot
-        [-q]                                        quiet
+    znap [-t tank/DATASET] -m "COMMIT_MESSAGE"       creates a snapshot
+         [-q]                                        quiet
+         [-r/-R]                                     recursive (default) / not recursive
 EOF
+}
+
+# $1: The target dataset full path
+# $2: The message
+znaplog(){
+echo -e >>"${ZNAPLOGFILE}" "$1$SUFFIX$SEP$2$BIGSEP"
 }
 
 # ---- Parsing ----
@@ -27,8 +40,9 @@ then
     exit 1
 fi
 
-verbosity=1
-while getopts ":t:hm:q" arg; do
+verbosity=2
+recursiveness=1
+while getopts ":t:hm:qrR" arg; do
     case $arg in
         t)
             target=${OPTARG}
@@ -39,6 +53,14 @@ while getopts ":t:hm:q" arg; do
         q)
             verbosity=0
             ;;
+        r)
+            recursiveness=1
+            set_r=1
+            ;;
+        R)
+            recursiveness=0
+            set_R=1
+            ;;
         h | *)
             usage
             exit 0
@@ -46,24 +68,42 @@ while getopts ":t:hm:q" arg; do
     esac
 done
 
+# verify that target and message are set
 if [ -z "$target" ] || [ -z "$commit_message" ]; then
     if [[ -z "$target" && ! -z "$commit_message" ]]; then
-        echo "You failed to specify a -t target."
+        if [[ $verbosity > 0 ]] ; then
+            echo "Defaulting to dataset ${DEFAULT_DATASET} because you did not specify a -t target."
+        fi
+        target=${DEFAULT_DATASET}
     fi
     if [[ ! -z "$target" && -z "$commit_message" ]]; then
         echo "You failed to specify a -m message."
+        usage
+        exit 1
     fi
+fi
 
+# verify that only one of -r -R are set
+if [[ ! -z $set_r && ! -z $set_R ]]; then
+    echo "You can only choose -r or -R, not both."
     usage
     exit 1
 fi
 
-# create log dir
-sudo mkdir -p 
 # --- Snapshot Creation ---
-if [[ $verbosity > 0 ]] ; then
-    echo -e "commit message:\t$commit_message"
+if [[ $verbosity > 1 ]] ; then
+    echo -e "message:\t$commit_message"
     echo -e "target:\t\t$target"
+    echo -e "suffix:\t\t$SUFFIX"
 fi
 
-sudo zfs snapshot 
+# store the commit messages first, so that they will be part of the snapshots
+# create log dir # TODO: place the logs actually in the dataset roots
+$sudo mkdir -p "./"
+$sudo touch "${ZNAPLOGFILE}"
+datasetpath="$target"
+znaplog "$datasetpath" "$commit_message"
+
+
+#sudo zfs snapshot 
+#TODO: actually run the snapshot
